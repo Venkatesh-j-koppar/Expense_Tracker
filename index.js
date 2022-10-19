d332152b9a036deef7d3e97d4d5f6493b5670c80
Venkatesh <venkateshkoppar@gmail.com>
2022-10-19 07:11:50 +0530

    Expense_tracker_complete

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-let expensesArray =[];
+let expensesArray = [];
 let balance = 0;
 let expense = 0;
 let transactionsection = document.getElementById("trlist");
@@ -6,7 +6,7 @@ let description = document.getElementById("descriptionofexpenses");
 let radiobutton = document.getElementsByName("expenseType");
 let amountgot = document.getElementById("amountgot");
 let totalbalance = document.getElementById("totalbalance");
-let errortag=document.querySelectorAll(".error")
+let errortag = document.querySelectorAll(".error");
 
 function showTransaction() {
   expensesArray.forEach((element, index) => {
@@ -34,32 +34,34 @@ function addTransaction() {
 }
 
 function save() {
-    let expenseObject = {
+  let expenseObject = {
     description: "",
     typeofExpense: "",
     amount: "",
-    };
-    
-    reset()
+  };
 
-    if (description.value.length == 0) {
+  reset();
+
+  if (description.value.length == 0) {
     let disdescriptionerror = document.getElementById("errordescriptionmsg");
     disdescriptionerror.innerText = "Description Is Empty";
     errortag[0].style.display = "block";
-    description.style.border= "solid 2px #E21717";
-   }
-    else if(radiobutton[0].checked==false && radiobutton[1].checked==false){
-        let diserroringainorexpensesmsg=document.getElementById("erroringainorexpensesmsg")
-        diserroringainorexpensesmsg.innerText="Please Select Type Of Transaction"
-        errortag[1].style.display = "block";
-    }
-  else if(amountgot.value.length==0){
-      let diserrorinamountmsg= document.getElementById("errorinamountmsg")
-      diserrorinamountmsg.innerText="Please Enter The Amount"
-      errortag[2].style.display = "block";
-      amountgot.style.border="solid 2px #E21717"
-  }
-  else {
+    description.style.border = "solid 2px #E21717";
+  } else if (
+    radiobutton[0].checked == false &&
+    radiobutton[1].checked == false
+  ) {
+    let diserroringainorexpensesmsg = document.getElementById(
+      "erroringainorexpensesmsg"
+    );
+    diserroringainorexpensesmsg.innerText = "Please Select Type Of Transaction";
+    errortag[1].style.display = "block";
+  } else if (amountgot.value.length == 0) {
+    let diserrorinamountmsg = document.getElementById("errorinamountmsg");
+    diserrorinamountmsg.innerText = "Please Enter The Amount";
+    errortag[2].style.display = "block";
+    amountgot.style.border = "solid 2px #E21717";
+  } else {
     document.getElementById("trlist").style.height = "150px";
     expenseObject.description = description.value;
     radiobutton.forEach((element) => {
@@ -82,9 +84,9 @@ function save() {
     });
     expenseObject.amount = amountgot.value;
     expensesArray.push(expenseObject);
-    
+
     showTransaction();
-    
+
     document.getElementById("formtofillforexpenses").style.display = "none";
   }
 }
@@ -95,25 +97,27 @@ function financialHealth(balance) {
   let statustextid = document.getElementById("statustext");
   totalbalance.innerHTML = `Balance &#8377 ${newBalance}`;
   if (expensePercentage > 70) {
-    document.querySelector(".financialStatus").style.backgroundColor ="#00D84A";
+    document.querySelector(".financialStatus").style.backgroundColor =
+      "#00D84A";
     statustextid.innerText = "Your Financial Condition Is Excellent";
-  }else if (expensePercentage > 40 && expensePercentage <= 70) {
-    document.querySelector(".financialStatus").style.backgroundColor ="#F4BE2C";
+  } else if (expensePercentage > 40 && expensePercentage <= 70) {
+    document.querySelector(".financialStatus").style.backgroundColor =
+      "#F4BE2C";
     statustextid.innerText = "Your Financial Condition Is Modrate";
   } else {
-    document.querySelector(".financialStatus").style.backgroundColor ="#BF3325";
+    document.querySelector(".financialStatus").style.backgroundColor =
+      "#BF3325";
     statustextid.innerText = "Your Financial Condition Is Week";
   }
 }
 
-function reset(){
-    errortag.forEach(element=>{
-        element.style.display="none";
-    })
-    
-    description.style.border= "solid 1px #000";
-    amountgot.style.border="solid 1px #000"
-}
+function reset() {
+  errortag.forEach((element) => {
+    element.style.display = "none";
+  });
 
+  description.style.border = "solid 1px #000";
+  amountgot.style.border = "solid 1px #000";
+}
 
 showTransaction();
